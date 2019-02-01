199e6d2297adbacf44d2eca12f6b892a528e6218
wiltrahan <twilorip@gmail.com>
2019-02-01 17:49:01 -0500

    inject dynamic values

--- src/app/challenges/challenge-tabs/challenge-tabs.component.ts
+++ src/app/challenges/challenge-tabs/challenge-tabs.component.ts
@@ -22,7 +22,7 @@ export class ChallengeTabsComponent implements OnInit {
       {
         relativeTo: this.active
       }
-    )
+    );
   };
 
 }
--- src/app/shared/ui/action-bar/action-bar.component.ts
+++ src/app/shared/ui/action-bar/action-bar.component.ts
@@ -16,7 +16,7 @@ export class ActionBarComponent implements OnInit {
   @Input() title: string;
   @Input() showBackButton = true;
   @Input() hasMenu = true;
-
+ 
   constructor(
     private page: Page, 
     private router: RouterExtensions,
