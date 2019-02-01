db0f40d1ff7c707c2a78fc18f977c7b177855164
wiltrahan <twilorip@gmail.com>
2019-02-01 16:42:47 -0500

    display sidedrawer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,36 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Component, OnInit, OnDestroy, ViewChild, AfterViewInit, ChangeDetectorRef } from "@angular/core";
 import { UIService } from "~/app/shared/ui.service";
 import { Subscription } from "rxjs";
+import { RadSideDrawerComponent } from "nativescript-ui-sidedrawer/angular/side-drawer-directives";
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 
 @Component({
     selector: "ns-app",
     moduleId: module.id,
     templateUrl: "./app.component.html",
 })
-export class AppComponent implements OnInit, OnDestroy { 
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy { 
+  
+  @ViewChild(RadSideDrawerComponent) drawerComponent: RadSideDrawerComponent;
   activeChallenge = '';
   private drawerSub: Subscription
-  constructor(private uiService: UIService) {
+  private drawer: RadSideDrawer;
+
+  constructor(private uiService: UIService, private changeDetectionRef: ChangeDetectorRef) {
 
   }
 
   ngOnInit() {
-   this.drawerSub = this.uiService.drawerState.subscribe(() => {
-     console.log('Toggled IT!****')
-   });
-   
+    this.drawerSub = this.uiService.drawerState.subscribe(() => {
+      if(this.drawer) {
+        this.drawer.toggleDrawerState();
+      }
+    });  
+  }
+
+  ngAfterViewInit() {
+    this.drawer = this.drawerComponent.sideDrawer;  
+    this.changeDetectionRef.detectChanges();
   }
 
   onChallengeInput(challengeDescription: string) {
