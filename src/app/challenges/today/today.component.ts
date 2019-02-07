e58952b7cc09a542ded72b42489f4bfe31ea2448
wiltrahan <twilorip@gmail.com>
2019-02-07 16:46:11 -0500

    populate the grid

--- src/app/challenges/today/today.component.ts
+++ src/app/challenges/today/today.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ns-today',
   templateUrl: './today.component.html',
-  styleUrls: ['./today.component.css'],
+  styleUrls: ['./today.component.scss'],
   moduleId: module.id,
 })
 export class TodayComponent implements OnInit {
